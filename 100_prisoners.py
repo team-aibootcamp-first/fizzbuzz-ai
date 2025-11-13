85c31b83d5aab6d8bce6dfd20a47972348d7d6fb
bawoodoll <bawoodoll@gmail.com>
2025-11-13 12:46:51 +0900

    WIP: Local changes to 100_prisoners before upstream merge

--- 100_prisoners.py
+++ 100_prisoners.py
@@ -12,7 +12,7 @@ def main():
     #     for _ in range(NUM_REPETITIONS):
     #         game = PrisionersGame(NUM_DRAWERS)
     #         
-    #         # num_victories += PrisionersGame.victory(game.play(approach))
+    #         # num_victories += PrisionersGame.victory(game.play(approach
 
     #     print('{:15}: {:5} ({:.2%})'.format(
     #         approach.__name__, num_victories, num_victories / NUM_REPETITIONS))
@@ -20,6 +20,18 @@ def main():
 
 class PrisionersGame:
     pass
-        
+
+@classmethod
+def victory(csl, results):
+    """Defines a victory of a game: all players won"""
+    return all(results)
+
+    approaches = [play_naive, play_naive_mem, play_optimum]
+
+def play(self, approach):
+    """Plays this game and returns a list of booleans with
+    True if a player one, False otherwise"""
+    return [approach(self, player) for player in self.drawer_ids]
+
 if __name__ == '__main__':
     main()
